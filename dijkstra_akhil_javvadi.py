64ea9aaf418a3ad0c5c60636da51c8c2163571c5
AkhilJavvadi <96479946+AkhilJavvadi@users.noreply.github.com>
2024-03-10 23:40:05 -0400

    Comments were added
    Explanation of function is done

--- dijkstra_akhil_javvadi.py
+++ dijkstra_akhil_javvadi.py
@@ -3,14 +3,14 @@ import numpy as np
 import time 
 from queue import PriorityQueue
 
-#creating the clearance and obstacle colour
+# Define clearance and obstacle colors
 clearance = 5 
-obstracle = (0,0,255)
+obstacle_color = (0,0,255)
 
-#defining Linear equation for obstacles
-def linear_equation(i,j,x1,y1,x2,y2,clearance):
+# Define linear equation for obstacles
+def linear_equation(i, j, x1, y1, x2, y2, clearance):
     if x2 - x1 == 0:
-        # handle case where the denominator is zero
+        # Handle case where the denominator is zero
         if i == x1:
             return j - y1
         else:
@@ -18,224 +18,174 @@ def linear_equation(i,j,x1,y1,x2,y2,clearance):
     else:
         return ((y2 - y1) / (x2 - x1 + clearance)) * (i - x1 - clearance/2) + y1 - j
 
-#Generating map
+# Generate map with obstacles
 def Map_Generator(height, width):
-
     map = np.zeros((height, width, 3))
     for i in range(map.shape[1]):
         for j in range(map.shape[0]):
+        # border
+            if(i>=0 and i<=5 and j>=0 and j<=500):
+                map[j][i] = obstacle_color
+        # border
+            if(i>=0 and i<=1200 and j>=0 and j<=5):
+                map[j][i] = obstacle_color
         #rectangle obstacle1
             if(i>=100 and i<=175 and j>=0 and j<=400):
-                map[j][i] = obstracle
+                map[j][i] = obstacle_color
         #rectangle obstacle 2
             if(i>=275 and i<=350 and j>=100 and j<=500):
-                map[j][i] = obstracle
+                map[j][i] = obstacle_color
         #hexagon obstacle
             if(linear_equation(i,j,*(650,100),*(780,175),clearance)<=0 and i<780 and linear_equation(i,j,*(780,325),*(650,400),clearance)>=0
            and linear_equation(i,j,*(650,400),*(520,325),clearance)>=0 and i>520 and linear_equation(i,j,*(520,175),*(650,100),clearance)<=0):
-                map[j][i] = obstracle
+                map[j][i] = obstacle_color
         #rectangle obstacle3
             if(i>=900 and i<=1020 and j>=50 and j<=125):
-                map[j][i] = obstracle
+                map[j][i] = obstacle_color
         #rectangle obstacle4
             if(i>=900 and i<=1020 and j>=375 and j<=450):
-                map[j][i] = obstracle
-        #rectangle obstacle1
+                map[j][i] = obstacle_color
+        #rectangle obstacle5
             if(i>=1020 and i<=1100 and j>=50 and j<=450):
-                map[j][i] = obstracle
+                map[j][i] = obstacle_color
+        # border
+            if(i>=1195 and i<=1200 and j>=0 and j<=500):
+                map[j][i] = obstacle_color
+        # border
+            if(i>=0 and i<=1200 and j>=495 and j<=500):
+                map[j][i] = obstacle_color
     return map
 
-#defining the obstacles
+# Check if a cell is an obstacle
 def isObstacle(map, x, y):
- 
-    if (map[x][y][2] < obstracle[2]):
+    if (map[x][y][2] < obstacle_color[2]):
         return False
     else:
         return True
     
-# Checking for applicable inputs 
+# Check input feasibility
 def checkInputFeasibility(x_start, y_start, x_goal, y_goal, map):
- 
     input_flag = True
-
-    if isObstacle(map, y_start, x_start):
-        print("!! Start Position is in an Obstacle/Wall, try again!")
+    if isObstacle(map, x_start, y_start):
+        print("Start Position is in an Obstacle/Wall, try again!")
         input_flag = False
-    if isObstacle(map, y_goal, x_goal):
-        print("!! Goal Position is in an Obstacle/Wall!, try again")
+    if isObstacle(map, x_goal, y_goal):
+        print("Goal Position is in an Obstacle/Wall!, try again")
         input_flag = False
-    
     return input_flag
 
-# Function to Move Top 
-def actionMoveTop(CurrentNode,map):
-
+# Function to move top
+def actionMoveTop(CurrentNode, map):
     NextNode = list(CurrentNode)
-    if(NextNode[1]-1 > 0) and (not isObstacle(map, NextNode[1]-1, NextNode[0])):
+    if (NextNode[1]-1 > 0) and (not isObstacle(map, NextNode[0], NextNode[1]-1)):
         Status = True
         NextNode[1] = NextNode[1] - 1 
     else:
         Status = False   
-
     return (Status, NextNode)
 
-# Function to Move diagonally top right of cost 1.4
-def actionMoveTopRight(CurrentNode,map):
-
+# Function to move diagonally top right
+def actionMoveTopRight(CurrentNode, map):
     NextNode = list(CurrentNode)
-    
-    if(NextNode[1]-1 > 0) and (NextNode[0]+1 <map.shape[1]) and (not isObstacle(map, NextNode[1]-1, NextNode[0])):
+    if (NextNode[1]-1 > 0) and (NextNode[0]+1 < map.shape[1]) and (not isObstacle(map, NextNode[0]+1, NextNode[1]-1)):
         Status = True
         NextNode[0] = NextNode[0] + 1 
         NextNode[1] = NextNode[1] - 1
     else:
         Status = False   
-
     return (Status, NextNode)
 
-# Function to Move Right diagonally
-def actionMoveRight(CurrentNode,map):
-    
+# Function to move right
+def actionMoveRight(CurrentNode, map):
     NextNode = list(CurrentNode)
-    if(NextNode[0]+1 <map.shape[1]) and (not isObstacle(map, NextNode[1], NextNode[0]+1)):
+    if (NextNode[0]+1 < map.shape[1]) and (not isObstacle(map, NextNode[0]+1, NextNode[1])):
         Status = True
         NextNode[0] = NextNode[0] + 1 
     else:
         Status = False   
-
     return (Status, NextNode)
 
-# Function to Move Bottom Right
-def actionMoveBottomRight(CurrentNode,map):
-    
+# Function to move bottom right
+def actionMoveBottomRight(CurrentNode, map):
     NextNode = list(CurrentNode)
-    if(NextNode[1]+1 < map.shape[0]) and (NextNode[0]+1 <map.shape[1]) and (not isObstacle(map, NextNode[1]+1, NextNode[0]+1)):
+    if (NextNode[1]+1 < map.shape[0]) and (NextNode[0]+1 < map.shape[1]) and (not isObstacle(map, NextNode[0]+1, NextNode[1]+1)):
         Status = True
         NextNode[0] = NextNode[0] + 1 
         NextNode[1] = NextNode[1] + 1
     else:
         Status = False   
-
     return (Status, NextNode)
 
-# Function to Move Bottom
-def actionMoveBottom(CurrentNode,map):
-    
+# Function to move bottom
+def actionMoveBottom(CurrentNode, map):
     NextNode = list(CurrentNode)
-    if(NextNode[1]+1 < map.shape[0]) and (not isObstacle(map, NextNode[1]+1, NextNode[0])):
+    if (NextNode[1]+1 < map.shape[0]) and (not isObstacle(map, NextNode[0], NextNode[1]+1)):
         Status = True 
         NextNode[1] = NextNode[1] + 1
     else:
         Status = False   
-
     return (Status, NextNode)
 
-# Function to Move Bottom Left(Diagonally)
-def actionMoveBottomLeft(CurrentNode,map):
-    
+# Function to move bottom left
+def actionMoveBottomLeft(CurrentNode, map):
     NextNode = list(CurrentNode)
-    if(NextNode[1]+1 < map.shape[0]) and (NextNode[0]-1 >0) and (not isObstacle(map, NextNode[1]+1, NextNode[0]-1)):
+    if (NextNode[1]+1 < map.shape[0]) and (NextNode[0]-1 > 0) and (not isObstacle(map, NextNode[0]-1, NextNode[1]+1)):
         Status = True 
         NextNode[0] = NextNode[0] - 1
         NextNode[1] = NextNode[1] + 1
     else:
         Status = False   
-
     return (Status, NextNode)
 
-# Function to Move Left
-def actionMoveLeft(CurrentNode,map):
-    
+# Function to move left
+def actionMoveLeft(CurrentNode, map):
     NextNode = list(CurrentNode)
-    if(NextNode[0]-1 > 0) and (not isObstacle(map, NextNode[1], NextNode[0]-1)):  
+    if (NextNode[0]-1 > 0) and (not isObstacle(map, NextNode[0]-1, NextNode[1])):  
         Status = True 
         NextNode[0] = NextNode[0] - 1
     else:
         Status = False   
-
     return (Status, NextNode)
 
-# Function to Move Top Left (Diagonally)
-def actionMoveTopLeft(CurrentNode,map):
-    
+# Function to move top left
+def actionMoveTopLeft(CurrentNode, map):
     NextNode = list(CurrentNode)
-    if(NextNode[1]-1 > 0) and (NextNode[0]-1 > 0) and (not isObstacle(map, NextNode[1]-1, NextNode[0]-1)):
+    if (NextNode[1]-1 > 0) and (NextNode[0]-1 > 0) and (not isObstacle(map, NextNode[0]-1, NextNode[1]-1)):
         Status = True 
         NextNode[0] = NextNode[0] - 1
         NextNode[1] = NextNode[1] - 1
     else:
         Status = False   
-
     return (Status, NextNode)
 
-#checking the goal node is reached
+# Check if goal node is reached
 def isGoalNode(CurrentNode, goalNode):
-
     if list(CurrentNode) == goalNode:
         return True
     else:
         return False
     
-# Implementing back_Tracking_Algo algorithm to trace the shortest path 
-def back_Tracking_Algo(goalNode, startNode, closed_list, map):
-    video_writer = cv.VideoWriter_fourcc(*'mp4v')
-    out = cv.VideoWriter('project2_1.mp4',video_writer,1000,(600,250)) # Saving the recorded video
-    
-    final_parent = closed_list.get(tuple(goalNode))   
-    cv.line(map, tuple(goalNode), tuple(final_parent), (255,0,0), 1)
-
-    parent_node_keys = closed_list.keys()
-    for key in parent_node_keys:
-        if key is not tuple(startNode):   
-            map[key[1]][key[0]] = [255,255,255]
-            cv.circle(map,tuple(startNode),5,(0,255,0),-1)
-            out.write(map)
-        cv.circle(map,tuple(goalNode),5,(0,255,0),-1)
-        
-        cv.imshow("Path Generation",map)
-        cv.waitKey(1)
-
-    while True:
-        key = closed_list.get(tuple(final_parent))    
-        
-        cv.line(map, tuple(key), tuple(final_parent), (255,0,0), 1)
-        out.write(map)
-
-        final_parent = key
-        
-        if key is startNode:
-            break
-
-    cv.imshow("Path Generation", map)
-    cv.waitKey(0)
-
+# Implement Dijkstra's algorithm to find shortest path
 def Dijkstra_algo(startNode, goalNode, map):
-    
     closed_list = {}    
     opened_list = PriorityQueue()    
-
     opened_list.put((0, startNode, startNode))
     visited = set()  # Track visited nodes to avoid duplicates
-    
     start_time = time.time()
     while not opened_list.empty():
-        
         cost_to_come, present_node, parent_node = opened_list.get()
         present_node = tuple(present_node)  # Convert list to tuple
         visited.add(present_node)  # Mark present node as visited
-        
         closed_list[(present_node[0], present_node[1])] = parent_node
-        
         if isGoalNode(present_node, goalNode):
-            print("\n Goal reached!")
+            print("\nGoal reached!")
             end_time = time.time()
             print("\nTime: "+str(round((end_time-start_time), 4)) + " [secs]")
             back_Tracking_Algo(goalNode, startNode, closed_list, map)
             return True
-
         for action in [actionMoveTop, actionMoveTopRight, actionMoveRight,
-               actionMoveBottomRight, actionMoveBottom, actionMoveBottomLeft,
-               actionMoveLeft, actionMoveTopLeft]:
+                       actionMoveBottomRight, actionMoveBottom, actionMoveBottomLeft,
+                       actionMoveLeft, actionMoveTopLeft]:
             flag, child_node = action(present_node, map)
             child_node_tuple = tuple(child_node)  # Convert list to tuple
             if flag is True and child_node_tuple not in visited:
@@ -252,16 +202,38 @@ def Dijkstra_algo(startNode, goalNode, map):
                     closed_list[child_node_tuple] = present_node
                     back_Tracking_Algo(goalNode, startNode, closed_list, map)
                     return True
-
-
-
     print("\n No path found between the start and goal explored_nodes") 
     return False
 
-# Calling the map generating functions 
+# Implement backtracking algorithm to trace the shortest path 
+def back_Tracking_Algo(goalNode, startNode, closed_list, map):
+    video_writer = cv.VideoWriter_fourcc(*'mp4v')
+    out = cv.VideoWriter('project2_1.mp4', video_writer, 1000, (600,250)) # Save the recorded video
+    final_parent = closed_list.get(tuple(goalNode))   
+    cv.line(map, tuple(goalNode), tuple(final_parent), (255,0,0), 1)
+    parent_node_keys = closed_list.keys()
+    for key in parent_node_keys:
+        if key is not tuple(startNode):   
+            map[key[1]][key[0]] = [255,255,255]
+            cv.circle(map, tuple(startNode), 5, (0,255,0), -1)
+            out.write(map)
+        cv.circle(map, tuple(goalNode), 5, (0,255,0), -1)
+        cv.imshow("Path Generation", map)
+        cv.waitKey(1)
+    while True:
+        key = closed_list.get(tuple(final_parent))    
+        cv.line(map, tuple(key), tuple(final_parent), (255,0,0), 1)
+        out.write(map)
+        final_parent = key
+        if key is startNode:
+            break
+    cv.imshow("Path Generation", map)
+    cv.waitKey(0)
+    out.release()
+
+# Main function
 if __name__ == '__main__':
-# display map with original obstracles  
-    map = Map_Generator(500, 1200)                 
+    map = Map_Generator(500, 1200)  # Generate map with obstacles               
     print('Enter the start position:')
     x_start = int(input("Enter your value of X-Axis: "))
     y_start = int(input("Enter your value of Y-Axis: "))
@@ -271,7 +243,5 @@ if __name__ == '__main__':
     if (checkInputFeasibility(x_start, y_start, x_goal, y_goal, map)):
         startNode = [x_start, y_start]
         goalNode = [x_goal, y_goal]
-
         res = Dijkstra_algo(startNode, goalNode, map)
-
         cv.destroyAllWindows()
